9a5d5654f9a3f8e81186dbb5351324d9da23043a
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-04 10:20:34 -0500

    feat(main): Add MVP

---
+++ main.ts
@@ -0,0 +1,91 @@
+import { format } from '@formkit/tempo';
+import { Plugin, TAbstractFile, TFile } from 'obsidian';
+
+export default class DenoteRenamer extends Plugin {
+	async onload(): Promise<void> {
+		this.registerEvent(
+			this.app.vault.on('modify', async (file: TAbstractFile) => {
+				if (!this.isTFile(file)) return;
+
+				await this.app.fileManager.processFrontMatter(
+					file,
+					async (frontmater: {
+						title: string;
+						tags: string[];
+						createdAt: string;
+						updatedAt: string;
+					}) => {
+						const excludeDirectories = '4-archives/templates/obsidian';
+						if (file.parent?.path === excludeDirectories) {
+							return;
+						}
+
+						if (Object.entries(frontmater).length === 0) return;
+
+						const renamedFile = this.getRenamedFilename({
+							fileExtension: file.extension,
+							fileBasename: frontmater.title,
+							tags: frontmater.tags,
+							timestamp: file.stat.ctime,
+						});
+
+						// TODO: Notifies, parent is possibly null
+						await this.app.fileManager.renameFile(
+							file,
+							`${file.parent?.path}/${renamedFile}`,
+						);
+					},
+				);
+			}),
+		);
+	}
+
+	isTFile(value: TAbstractFile): value is TFile {
+		return 'stat' in value;
+	}
+
+	getRenamedFilename(payload: {
+		timestamp: number;
+		tags: string[] | string;
+		fileBasename: string;
+		fileExtension: string;
+	}): string {
+		const slug = format(new Date(payload.timestamp), 'YYYYMMDDThhmmss');
+		const formattedFilename = this.formatToKebabCase(
+			String(payload.fileBasename),
+		);
+		const formattedTags = this.getFormatedTags(payload.tags);
+
+		return `${slug}--${formattedFilename}${formattedTags}.${payload.fileExtension}`;
+	}
+
+	formatToKebabCase(payload: string): string {
+		return payload
+			.replace(/([a-z])([A-Z])/g, '$1-$2')
+			.replace(/[\s_]+/g, '-')
+			.replace(/\//g, '-')
+			.toLowerCase();
+	}
+
+	getFormatedTags(tags?: string[] | string): string {
+		switch (typeof tags) {
+			case 'undefined':
+				return '';
+
+			case 'string':
+				return `__${tags}`;
+
+			case 'object':
+				if (Array.isArray(tags)) {
+					const formattedTags = tags.map((tag) => this.formatToKebabCase(tag));
+
+					return `__${formattedTags.join('_')}`;
+				}
+
+				return '';
+
+			default:
+				return '';
+		}
+	}
+}
