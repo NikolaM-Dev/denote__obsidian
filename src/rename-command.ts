43fd27e228a73368f90512ad752d9d0f268f7cbb
Juan David Merchan Torres <juan1014166@gmail.com>
2025-08-08 16:31:22 -0500

    feat(rename-command): Add headless renameFile functionality

---
+++ src/rename-command.ts
@@ -0,0 +1,24 @@
+export async function renameFile(app: App): Promise<void> {
+  const file = app.workspace.getActiveFile();
+
+  if (!file) return log('File not found');
+
+  const frontMatter = await getFrontMatter(app, file);
+  await setFrontMatter(app, file, frontMatter);
+
+  const title = frontMatter.aliases[0];
+
+  if (!title)
+    return notify('⚠️ Alias not found, please add at least one alias');
+
+  const slug = getSlug(title);
+  const formattedTags = getFormatTags(frontMatter.tags);
+
+  const newFilename = `${frontMatter.id}--${slug}${formattedTags}.${file.extension}`;
+
+  if (!file.parent) return notify("⚠️ File doesn't have file.parent");
+
+  const newPath = `${file.parent.path}/${newFilename}`;
+
+  await this.app.fileManager.renameFile(file, newPath);
+}
