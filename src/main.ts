8b1d4c18e3b7d8b12acb6c3d7bcaf83d50cc6000
Juan David Merchan Torres <juan1014166@gmail.com>
2025-08-17 13:54:13 -0500

    feat(main): Add new commands and event setup

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { App, Plugin } from 'obsidian';
 
 import { getFormatFrontMatterCommand } from './format-front-matter-command';
+import { getFormatHeadings } from './format-headings-command';
+import { getTitleSchemeRenameFileCommand } from './title-scheme-rename-command';
 import { log } from './log';
+import { onVerifyFilename } from './on-verify-filename';
 
 export default class Denote extends Plugin {
   readonly app: App;
@@ -10,6 +13,10 @@ export default class Denote extends Plugin {
     log('Denote is on');
 
     this.addCommand(getFormatFrontMatterCommand(this.app));
+    this.addCommand(getFormatHeadings(this.app));
+    this.addCommand(getTitleSchemeRenameFileCommand(this.app));
+
+    this.app.metadataCache.on('changed', onVerifyFilename);
   }
 
   async onunload(): Promise<void> {
