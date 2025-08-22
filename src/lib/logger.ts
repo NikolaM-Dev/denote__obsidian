2dda70392f25d8c1365ce37094453811e7b0e66a
Juan David Merchan Torres <juan1014166@gmail.com>
2025-08-22 07:59:05 -0500

    feat(lib): Add new logger

---
+++ src/lib/logger.ts
@@ -0,0 +1,37 @@
+import { Notice } from 'obsidian';
+import { removeConsecutiveSpaces } from './string';
+
+interface ILogPayload {
+  extra?: string;
+  icon?: string;
+  msg: string;
+  showToast?: boolean;
+}
+
+const DEBUG_MODE = true;
+const PLUGIN_PREFIX = '[DENOTE]:';
+
+export const logger = {
+  debug: (payload: ILogPayload) => log({ icon: '🐞', ...payload }),
+  error: (payload: ILogPayload) => log({ icon: '❌', ...payload }),
+  info: (payload: ILogPayload) => log({ icon: 'ℹ️', ...payload }),
+  warn: (payload: ILogPayload) => log({ icon: '⚠️', ...payload }),
+};
+
+function toast(msg: string, icon?: string): void {
+  if (!DEBUG_MODE) return;
+
+  const message = removeConsecutiveSpaces(`${PLUGIN_PREFIX} ${icon} ${msg}`);
+
+  new Notice(message);
+}
+
+function log(payload: ILogPayload): void {
+  if (!DEBUG_MODE) return;
+  const { extra, icon, msg, showToast = true } = payload;
+
+  console.log(`${PLUGIN_PREFIX} ${msg}`);
+
+  if (extra) console.log({ extra });
+  if (showToast) toast(msg, icon);
+}
