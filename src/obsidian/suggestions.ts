6903b4a54dfe7b854ffcb904ef765c89e1e0ba2e
Juan David Merchan Torres <juan1014166@gmail.com>
2025-08-26 08:15:02 -0500

    feat(suggestions): Add `menu` to TARGET_CLASSES

--- src/obsidian/suggestions.ts
+++ src/obsidian/suggestions.ts
@@ -1,4 +1,4 @@
-const TARGET_CLASSES = ['suggestion-container', 'modal-container'];
+const TARGET_CLASSES = ['menu', 'modal-container', 'suggestion-container'];
 
 export function getIsSuggestionElementActive(): boolean {
   return TARGET_CLASSES.some((className) =>
