1ad4b2d2a080bf2343130d48f3f8c002b3822991
Juan David Merchan Torres <juan1014166@gmail.com>
2025-08-26 11:47:57 -0500

    refactor(context): Change to use static class methods

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -1,27 +1,22 @@
 import { App } from 'obsidian';
 import { err, ok, Result } from 'neverthrow';
 
-interface ICtxValues {
-  app: App | null;
-}
 
-interface ICtxMethods {
-  getApp: () => Result<App, string>;
-  setApp: (app: App | null) => void;
-}
+export class ctx {
+  private static _app: App | null = null;
+  private static _headingsCache: IHeadingsCache = {};
 
-type ICtx = ICtxValues & ICtxMethods;
+  static getApp(): Result<App, string> {
+    if (!this._app) {
+      return err(
+        "App wasn't assigned. Ensure the app is properly initialized before accessing this context.",
+      );
+    }
+    return ok(this._app);
+  }
 
-export const ctx: ICtx = {
-  app: null,
+  static setApp(app: App | null): void {
+    this._app = app;
+  }
 
-  getApp: (): Result<App, string> => {
-    if (!ctx.app) return err("App wasn't assigned");
 
-    return ok(ctx.app);
-  },
-
-  setApp: (app: App | null): void => {
-    ctx.app = app;
-  },
-};
