7e9c951ba03fd8f0995880df2e97fcbf7974b2de
Juan David Merchan Torres <juan1014166@gmail.com>
2025-08-27 13:51:52 -0500

    ci(root): Add `svu` and a pre-hook to remind me to tag

--- bump_version.sh
+++ bump_version.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
 function main() {
+	next_version="v$(jq -r '.version' package.json)"
+	current_version="$(svu current)"
+	if [[ $current_version == $next_version ]]; then
+		echo "Current version is the same as the next version"
+		exit 1
+	fi
+
 	bun run version-bump.mjs
 	bun run format:fix
 	git add manifest.json versions.json package.json
-	git commit -m "ci(root): Change to version $(jq -r '.version' package.json)"
+	git commit -m "ci(root): Change to version $next_version"
+	git tag $next_version
+	git push --tags
 }
 
 main
