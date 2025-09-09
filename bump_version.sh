ba63eda9b7d750d81276f0507427980f3a4f3489
Juan David Merchan Torres <juan1014166@gmail.com>
2025-09-08 21:36:47 -0500

    feat(bump_version): Change to automatically update package.json and push the tag

--- bump_version.sh
+++ bump_version.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
 function main() {
-	next_version="v$(jq -r '.version' package.json)"
-	current_version="$(svu current)"
+	current_version=$(svu current)
+	next_version=$(svu next)
+
 	if [[ $current_version == $next_version ]]; then
 		echo "Current version is the same as the next version"
 		exit 1
 	fi
 
+	package_json_current_version=${current_version/v/}
+	package_json_next_version=${next_version/v/}
+	sed -i "s/$package_json_current_version/$package_json_next_version/" ./package.json
 	bun run version-bump.mjs
 	bun run format:fix
 	git add manifest.json versions.json package.json
 	git commit -m "ci(root): Change to version $next_version"
 	git tag $next_version
-	git push --tags
+	git push origin HEAD --tags
 }
 
 main
